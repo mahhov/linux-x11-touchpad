c2dd496dbcb09e47c4bc81d047d3ffaa40698313
mahhov1 <mahhov1@gmail.com>
2018-06-23 15:35:21 -0700

    minor clean up scroll

--- src/ScrollHandler.cpp
+++ src/ScrollHandler.cpp
@@ -4,13 +4,18 @@
 static Point centerShift = Point::invalidPoint; // todo cleanup
 static Smoother curvatureSmoother{.5};
 static bool line;
-static double x;
 static Point base = Point::invalidPoint, doubleBase = Point::invalidPoint;
 static Smoother smootherLastScroll{.13};
 
 static Smoother alcs{.1};
 static Smoother accs{.1};
 static Smoother acs{.1};
+
+double minDistance = .005, minDistanceSq = minDistance * minDistance;
+double lineScale = 12.5;
+double circleScale = 4.25;
+double lineModeChangeResistance = 1.5;
+double transformMin = .15;
 
 ScrollHandler::ScrollHandler(int delta, double boundary, double threshold, double smoothness) :
         delta(delta),
@@ -68,86 +73,43 @@ void ScrollHandler::iterate(TouchHistory history, TouchController &controller, P
         doubleBase = last;
         return;
     }
-
-    double minDistance = .005, minDistanceSq = minDistance * minDistance;
-
     if (base.invalid) {
         if (!(last - doubleBase) > minDistance)
             base = last;
         return;
     }
-
     if (~lastRelBase < minDistanceSq) {
-        smootherLastScroll.smooth(0);
+        smootherLastScroll.add(0);
         return;
     }
 
     double cross = lastRelBase * baseRelDoubleBase / !lastRelBase / !baseRelDoubleBase;
-    double curvature = fabs(cross / fmax(!lastRelDoubleBase, .00001));
+    double lineChange = lastRelDoubleBase * doubleBaseRelCenter / !doubleBaseRelCenter * lineScale;
+    double circleChange = cross * circleScale;
+    double absLineChange = alcs.smooth(fabs(lineChange));
+    double absCircleChange = accs.smooth(fabs(circleChange));
 
-    curvature = curvatureSmoother.smooth(curvature, .1);
+    if (line)
+        line = absCircleChange < absLineChange * lineModeChangeResistance;
+    else
+        line = absLineChange > absCircleChange * lineModeChangeResistance;
 
-    // todo change baseRelCenter to doubleBaseRelCenter
-    double lineChange = lastRelDoubleBase * doubleBaseRelCenter / !doubleBaseRelCenter * 12.5;
-    double circleChange = cross * 4.25;
-    double absLineChange = fabs(lineChange / !lastRelDoubleBase / 40);
-    double absCircleChange = fabs(circleChange);
+    double change = smoother.smooth(line ? lineChange : circleChange);
 
-    absLineChange = alcs.smooth(absLineChange);
-    absCircleChange = accs.smooth(absCircleChange);
-
-    double s = 1.5;
-
-    if (line && absCircleChange > absLineChange * s)
-        line = false;
-    else if (!line && absLineChange > absCircleChange * s)
-        line = true;
-
-    double change;
     if (!line) {
-        change = smoother.smooth(circleChange);
         centerShift = change < 0 ? ++lastRelDoubleBase : --lastRelDoubleBase;
         centerShift = centerShift / !centerShift * .2;
-    } else
-        change = smoother.smooth(lineChange);
-
+    }
     center = base + centerShift;
 
-    double min = .15;
-    if (change > min)
-        change = (change - min) * 2 + min;
-    else if (change < -min)
-        change = (change + min) * 2 - min;
-
-    // todo adjust the change transform
+    if (change > transformMin) // todo adjust the transform
+        change = (change - transformMin) * 2 + transformMin;
+    else if (change < -transformMin)
+        change = (change + transformMin) * 2 - transformMin;
 
     smootherLastScroll.add(change);
-    x += change;
 
     controller.scroll(accumulator.accumulate(change));
-
-    if (x > 50)
-        x -= 100;
-    if (x < -50)
-        x += 100;
-
-    paint.addPoint(base);
-    paint.addPoint(doubleBase);
-    paint.addPoint(last);
-    paint.addPoint(last - lastRelBase * .1 / !lastRelBase); // base tail
-    paint.addPoint(center);
-
-    paint.addPoint({.08, Paint::scale(change, .3)});
-    paint.addPoint({.15, Paint::scale(curvature, 1. / 80)});
-    paint.addPoint({.28, Paint::scale(x, .01)});
-    paint.addPoint({0, Paint::scale(0, 1)});
-    paint.addPoint({boundary, 0});
-    paint.addPoint({boundary, 1});
-
-    paint.addPoint({.35, Paint::scale(lineChange, .25)});
-    paint.addPoint({.38, Paint::scale(circleChange, .25)});
-    paint.addPoint({.45, Paint::scale(absLineChange, .25)});
-    paint.addPoint({.48, Paint::scale(absCircleChange, .25)});
 
     doubleBase = base;
     base = last;
